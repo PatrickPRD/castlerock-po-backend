6172cfb7f5275f52be2cbb8e108dfc1c950d6670
PatrickPRD <patrick@prd.ie>
2026-01-06 11:13:17 +0000

    highlight open row in locations

--- public/location-report.js
+++ public/location-report.js
@@ -52,7 +52,7 @@ async function loadReport() {
   // DETAILS ROW (STAGES)
   table.innerHTML += `
     <tr class="details-row" id="${rowId}" style="display:none">
-      <td colspan="4">
+      <td colspan="5">
         <table class="inner-table">
           <thead>
             <tr>
